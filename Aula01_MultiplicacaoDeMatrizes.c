ed39b4cba47b6f7841f88cb4322afa429c81c29c
Pietra Paz <81472166+pietrapaz@users.noreply.github.com>
2024-08-09 09:38:25 -0300

    add file

---
+++ Aula01_MultiplicacaoDeMatrizes.c
@@ -0,0 +1,16 @@
+// multiplicacao de matriz //
+
+#include <stdio.h>
+
+int main(){
+	for (i = 0; i < 2; i++){
+		for (j = 0; j < 2; j++){
+			for (k = 0; k < 3; k++){
+				resultado[i][j] += (operando1[i][k] * operando2[k][j]);
+			}
+		}
+	}
+
+}
+
+
